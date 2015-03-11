934dcb5b6c33018e4c0aa771dd8f14a42d52993b
Ryan Castner <audiolion@users.noreply.github.com>
2015-03-11 16:52:30 -0400

    Create ConnectMySQLServer.java
    haven't updated comments, added some new methods, added connections for default localhost, still need to obfuscate this data

---
+++ ConnectMySQLServer.java
@@ -0,0 +1,233 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+/**
+ * Class is set up to connect to a MySQL Server database and provides methods
+ * to perform CRUD operations on the database it connects to. Dependent on the
+ * mysql-connector-java-5.0.8-bin.jar driver being in the classpath.
+ * 
+ * @author Ryan Castner rrc9704@rit.edu
+ *
+ */
+
+public class ConnectMySQLServer {
+		
+	//Connection attribute
+	private Connection conn = null;
+	
+	private static final String DEFAULT_URL = "jdbc:mysql://localhost/travel2";
+	private static final String DEFAULT_USER = "root";
+	private static final String DEFAULT_PASS = "student";
+	
+	private static class SingletonWrapper{
+		private static ConnectMySQLServer INSTANCE = new ConnectMySQLServer();
+	}
+	
+	public static ConnectMySQLServer getInstance(){
+		return SingletonWrapper.INSTANCE;
+	}
+	
+	private ConnectMySQLServer(){
+	}
+	
+	/**
+	 * Attempts to connect to a MS SQL Server database with the provided
+	 * parameter information. Utilizes the sqljdbc4.jar driver. Returns
+	 * a boolean result indicating connection success.
+	 * 
+	 * @param dbConnectURL - db url of the form jdbc:subprotocol:subname
+	 * @param username - username for login credentials
+	 * @param password - password for login credentials
+	 * @return boolean - true if connection was successful, false if not
+	 */
+	public boolean dbConnect(String dbConnectURL, String username, String password){
+	      try{
+	         Class.forName("com.mysql.jdbc.Driver");
+	         conn = DriverManager.getConnection(dbConnectURL, username, password);
+	      }catch(ClassNotFoundException ex){
+	    	  ex.printStackTrace();
+	      }catch(SQLException e){
+	    	  e.printStackTrace();
+	      }
+	      if(conn != null){
+	    	  return true;
+	      }else{
+	    	  return false;
+	      }
+	}
+	
+	/**
+	 * Closes the current database connection if it is open and sets the
+	 * attribute to null.
+	 */
+	public void dbConnectClose(){
+		if(conn != null){
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		conn = null;
+	}
+	
+	/**
+	 * Method executes a SQL Query if a database connection exists and converts
+	 * the result set into a 2D ArrayList that is returned.
+	 * 
+	 * @param sql - String representing a SQL Query
+	 * @return 2D ArrayList, if empty, connection did not exist, or no results
+	 */
+	public ArrayList<ArrayList<String>> getData(String sql){
+		if(conn == null){
+			return new ArrayList<ArrayList<String>>();
+		}
+		
+		Statement stmt = null;
+		ResultSet rs = null;
+		ResultSetMetaData metaData = null;
+		int columnCount = 0;
+		ArrayList<ArrayList<String>> resultSet = new ArrayList<ArrayList<String>>();
+
+		try{
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			metaData = rs.getMetaData();
+			columnCount = metaData.getColumnCount();
+			while(rs.next()){
+				ArrayList<String> list = new ArrayList<String>();
+				for(int i = 1; i < columnCount; i++){
+					String str = rs.getString(i);
+					list.add(str);
+				}
+				resultSet.add(list);
+			}
+		}catch (SQLException e){
+			if(resultSet.isEmpty()){
+				e.printStackTrace();
+			}else{
+				e.printStackTrace();
+			}
+		}	
+		return resultSet;
+	}
+	
+	/**
+	 * Grabs the raw result set from a SQL query. Methods using this method
+	 * will need to perform error checking.
+	 * 
+	 * @param sql - SQL Query
+	 * @return rs - the result set from the query, null if no connection to db
+	 */
+	public ResultSet getResultSet(String sql){
+		if(conn == null){
+			return null;
+		}
+		
+		Statement stmt = null;
+		ResultSet rs = null;
+		
+		try{
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		
+		return rs;
+	}
+	
+	/**
+	 * Grabs the raw result set meta data from a SQL query. Methods using this
+	 * method will need to perform error checking.
+	 * 
+	 * @param sql - SQL Query
+	 * @return metaData - Meta data of the result set, null if no db connection
+	 */
+	public ResultSetMetaData getMetaData(String sql){
+		if(conn == null){
+			return null;
+		}
+		
+		Statement stmt = null;
+		ResultSet rs = null;
+		ResultSetMetaData metaData = null;
+		
+		try{
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			metaData = rs.getMetaData();
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		
+		return metaData;		
+	}
+	
+	/**
+	 * Method determines whether or not a SQL Query will return a result set
+	 * 
+	 * @param sql - a String representing a SQL Query
+	 * @return - A boolean value, true indicates a result set was returned
+	 */
+	public boolean setData(String sql){
+		if(conn == null){
+			return false;
+		}
+		
+		Statement stmt = null;
+		boolean result = false;
+		try{
+			stmt = conn.createStatement();
+			result = stmt.execute(sql);
+		}catch(SQLException e){
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * Sets up the default connection string to RIT's database. If args are
+	 * provided then it will use them instead.
+	 * @param args - command line arguments
+	 */
+    public static void main(String[] args){
+    	String url = "";
+    	String username = "";
+    	String password = "";
+    	if(args.length == 3){
+    		url = args[0];
+    		username = args[1];
+    		password = args[2];
+    	}
+    	ConnectMySQLServer connServer = new ConnectMySQLServer();
+    	connServer.dbConnect(DEFAULT_URL, DEFAULT_USER, DEFAULT_PASS);
+    	String sql = "SELECT * from Equipment";
+    	ArrayList<ArrayList<String>> data = connServer.getData(sql);
+    	for(ArrayList<String> array : data){
+    		for(String item : array){
+    			System.out.print("\t"+item);
+    		}
+    		System.out.println();
+    	}
+    	ResultSetMetaData metaData = connServer.getMetaData(sql);
+    	int columnCount;
+		try {
+			columnCount = metaData.getColumnCount();
+			for(int i = 1; i <= columnCount; i++){
+	    		System.out.println(metaData.getColumnName(i));
+	    	}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+    	
+    	connServer.dbConnectClose();
+    }
+}
